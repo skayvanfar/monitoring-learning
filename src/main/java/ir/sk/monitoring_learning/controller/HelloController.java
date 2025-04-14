f4e26791a3d20fbb9d51c020ebb0d254a5c04c8f
Saeid Keyvanfar <kayvanfar.sj@gmail.com>
2025-04-15 00:13:04 +0200

    add logging and configure ELK

--- src/main/java/ir/sk/monitoring_learning/controller/HelloController.java
+++ src/main/java/ir/sk/monitoring_learning/controller/HelloController.java
@@ -1,9 +1,11 @@
 package ir.sk.monitoring_learning.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import ir.sk.monitoring_learning.service.MetricsService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class HelloController {
 
@@ -15,6 +17,7 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String sayHello() {
+        log.info("Hello endpoint was called.");
         metricsService.countHelloRequest();
         return "Hello, World!";
     }
