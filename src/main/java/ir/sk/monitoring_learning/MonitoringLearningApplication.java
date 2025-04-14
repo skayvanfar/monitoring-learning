cd2ad41a04cf737b3fd90b5b4d3ff05e63d39748
Saeid Keyvanfar <kayvanfar.sj@gmail.com>
2025-04-14 18:08:45 +0200

    creat base spring boot service

---
+++ src/main/java/ir/sk/monitoring_learning/MonitoringLearningApplication.java
@@ -0,0 +1,13 @@
+package ir.sk.monitoring_learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitoringLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonitoringLearningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ir/sk/monitoring_learning/MonitoringLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.sk.monitoring_learning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitoringLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
